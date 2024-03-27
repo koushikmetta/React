536656a795a805c2406e5bc624bb87e7d2828f07
koushikmetta <koushikmetta98@gmail.com>
2024-03-27 19:18:31 -0400

    updated

---
+++ timer-game/src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const input = useRef();
+  const [playername, setPlayerName] = useState(null);
+
+
+  function handleClick() {
+    setPlayerName(input.current.value);
+    input.current.value = '';
+  }
+  return (
+
+    <section id="player">
+      <h2>Welcome {playername ?? "unknown entity"}</h2>
+      <p>
+        <input ref={input} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+
+  );
+}
---
+++ timer-game/src/components/ResultModal.jsx
@@ -0,0 +1,36 @@
+import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom';
+
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainTime, onReset }, ref) {
+    const dialog = useRef();
+    const userLost = remainTime <= 0;
+    const formattedTime = (remainTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainTime / (targetTime * 1000)) * 100);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        }
+
+    });
+    return createPortal(
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+            <p>
+                Stopped the timer with <strong>{formattedTime} seconds left.</strong>
+            </p>
+
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById("modal")
+    );
+})
+export default ResultModal;
